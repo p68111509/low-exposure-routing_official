0fd5bb38aabe74b680f3eb5955766984a71a3fee
p68111509 <p68111509@gs.ncku.edu.tw>
2025-04-06 20:31:02 +0800

    主程式更新

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ with col1:
     with subtile_row[0]:
         st.markdown(
             """
-            <h3 style='font-family: PingFang TC;'>
+            <h3 style='font-family: "Noto Sans TC", "PingFang TC", "Microsoft JhengHei", sans-serif;'>
                 輸入地址 <span style='font-size: 0.8em; color: #bbbbbb;'>(或於地圖雙擊)</span>
             </h3>
             """,
