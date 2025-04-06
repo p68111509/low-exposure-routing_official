1cd9855d3d4b17d835b6e589468488de36958b48
p68111509 <p68111509@gs.ncku.edu.tw>
2025-04-06 20:17:43 +0800

    主程式更新

--- app.py
+++ app.py
@@ -224,7 +224,7 @@ with col1:
         </style>
     """, unsafe_allow_html=True)
 
-    btn_row = st.columns([15, 15, 15, 40, 20])
+    btn_row = st.columns([15, 15, 15, 40, 30])
 
     with btn_row[0]:
         if st.button("機車"):
