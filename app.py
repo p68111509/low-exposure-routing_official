68709e01e304067d37190f06ad7ba869444d42a7
p68111509 <p68111509@gs.ncku.edu.tw>
2025-04-07 13:47:55 +0800

    更新

--- app.py
+++ app.py
@@ -143,17 +143,17 @@ with col1:
                 margin-bottom: 4px;
                 text-shadow: 1px 1px 2px rgba(0,0,0,0.1);
             ">
-                Geo-AI 路徑好空氣喵喵
+                Geo-AI 路徑好空氣
             </h1>
             <div style="
                 font-family: 'Noto Sans TC', 'PingFang TC', 'Microsoft JhengHei', sans-serif;
-                font-size: 14px;
+                font-size: 18px;
                 font-weight: 500;
                 color: #666666;
                 text-align: center;
                 margin-bottom: 0.5em;
             ">
-                台北市 & 新北市喵喵喵喵
+                台北市 & 新北市
             </div>
         """, unsafe_allow_html=True)
 
