dd1028322c982eda41a421c882cc4ec6abb1b26a
p68111509 <p68111509@gs.ncku.edu.tw>
2025-04-08 00:19:44 +0800

    更新

--- app.py
+++ app.py
@@ -681,3 +681,14 @@ with col3:
 #         <img class="logo-img" src="data:image/jpg;base64,{logo2_base64}"><br><br>
 #     </div>
 # """, unsafe_allow_html=True)
+
+st.markdown("""
+    <hr style="margin-top: 40px; margin-bottom: 10px; border: none; border-top: 1px solid #ccc;" />
+
+    <div style="text-align: center; font-size: 13px; color: #666; font-family: 'Noto Sans TC', 'Microsoft JhengHei', sans-serif;">
+        © 2025 許家瑋｜國立成功大學 測量及空間資訊學系<br>
+        聯絡信箱：<a href="mailto:p68111509@gs.ncku.edu.tw">p68111509@gs.ncku.edu.tw</a>｜GitHub 專案：<a href="https://github.com/p68111509/low-exposure-routing_demo" target="_blank">low-exposure-routing_demo</a><br><br>
+        本系統資料與分析目的僅供學術與教育參考，部分 PM2.5 空間資訊參考自環境部公開資料。
+    </div>
+""", unsafe_allow_html=True)
+
