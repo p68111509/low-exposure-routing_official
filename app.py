a33e9dbb7ebfe3f471a5ba9ccd9b18dcf943af90
p68111509 <p68111509@gs.ncku.edu.tw>
2025-04-07 13:49:48 +0800

    更新

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ with col1:
                 color: #444444;
                 text-align: center;
             '>
-                輸入地址 <span style='font-size: 0.8em; color: #bbbbbb;'>(或於地圖雙擊)</span>
+                輸入地址/於地圖雙擊
             </div>
             """,
             unsafe_allow_html=True
