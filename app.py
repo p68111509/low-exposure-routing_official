09d029764c400ee4d1a92c9f2717ed63f8fb95dc
p68111509 <p68111509@gs.ncku.edu.tw>
2025-04-08 21:43:07 +0800

    更新

--- app.py
+++ app.py
@@ -689,7 +689,7 @@ st.markdown("""
 
     <div style="text-align: center; font-size: 13px; color: #666; font-family: 'Noto Sans TC', 'Microsoft JhengHei', sans-serif;">
         <p style="margin-bottom: 4px;">
-            © 2025 許家瑋｜國立成功大學 測量及空間資訊學系｜指導老師：吳治達 教授<br>
+            © 2025 許家瑋<br>
             聯絡信箱：<a href="mailto:p68111509@gs.ncku.edu.tw">p68111509@gs.ncku.edu.tw</a>
         </p>
         <p style="margin-top: 6px; margin-bottom: 0px;">
@@ -701,3 +701,5 @@ st.markdown("""
         </p>
     </div>
 """, unsafe_allow_html=True)
+
+# © 2025 許家瑋 林侑萱 林宇家｜國立成功大學 測量及空間資訊學系｜指導老師：吳治達 教授
